4705d39efbdddddf08187c6c6064d2482aa53abf
Dustin Bachrach <ahdustin@gmail.com>
2013-10-14 23:26:06 -0500

    Adding Example back

---
+++ OCUDLExample/OCUDLExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OCUDLExample
+//
+//  Created by DUSTIN on 10/14/13.
+//  Copyright (c) 2013 Dustin Bachrach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
