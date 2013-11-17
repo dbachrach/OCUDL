e56f68351b97c161a560418d1616b81a0957817b
Klaus-Peter Dudas <klaus@hypercrypt.net>
2013-11-17 18:45:23 +0000

    Removed reliance on `stringWithUTF8String:`

--- Classes/OCUDLManager.m
+++ Classes/OCUDLManager.m
@@ -11,14 +11,6 @@
 #import <objc/runtime.h>
 #import <objc/message.h>
 
-void Swizzle(Class c, SEL orig, SEL new)
-{
-    Method origMethod = class_getClassMethod(c, orig);
-    Method newMethod = class_getClassMethod(c, new);
-	method_exchangeImplementations(origMethod, newMethod);
-}
-
-
 @implementation OCUDLManager
 
 static dispatch_once_t s_pred;
@@ -39,10 +31,6 @@ static OCUDLManager *s_manager = nil;
 	{
 		self.prefixMapping = [[NSMutableDictionary alloc] init];
 		self.suffixMapping = [[NSMutableDictionary alloc] init];
-
-		Swizzle([NSString class],
-			@selector(stringWithUTF8String:),
-			@selector(ocudlStringWithUTF8String:));
 	}
 	return self;
 }
@@ -65,6 +53,65 @@ static OCUDLManager *s_manager = nil;
 - (void)registerSuffix:(NSString*)suffix forBlock:(OCUDLBlock)block
 {
 	self.suffixMapping[suffix] = block;
+}
+
+#pragma mark - Object Emitter
+
++ (id)objectWithUTF8String:(const char *)nullTerminatedCString
+{
+	NSString *str = [NSString stringWithUTF8String:nullTerminatedCString];
+	
+	NSMutableDictionary *prefixMapping = [OCUDLManager defaultManager].prefixMapping;
+	NSArray *sortedPrefixMappingKeys = [[prefixMapping allKeys] sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+		return[@([obj1 length]) compare:@([obj2 length])];
+	}];
+	
+	for (NSString *prefix in sortedPrefixMappingKeys) {
+		if ([str hasPrefix:prefix]) {
+			
+			str = [str substringFromIndex:[prefix length]];
+			
+			id mapping = prefixMapping[prefix];
+			
+			// http://stackoverflow.com/questions/6536244/check-if-object-is-class-type
+			if (class_isMetaClass(object_getClass(mapping))) {
+				Class class = mapping;
+				id<OCUDLClass> literalClass = [class alloc];
+				return (id)[literalClass initWithLiteral:str prefix:prefix];
+			}
+			else if ([mapping isKindOfClass:NSClassFromString(@"NSBlock")]) {
+				OCUDLBlock block = mapping;
+				return block(str, prefix);
+			}
+		}
+	}
+	
+	NSMutableDictionary *suffixMapping = [OCUDLManager defaultManager].suffixMapping;
+	NSArray *sortedSuffixMappingKeys = [[suffixMapping allKeys] sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+		return[@([obj2 length]) compare:@([obj1 length])];
+	}];
+	
+	for (NSString *suffix in sortedSuffixMappingKeys) {
+		if ([str hasSuffix:suffix]) {
+			
+			str = [str substringToIndex:[str length] - [suffix length]];
+			
+			id mapping = suffixMapping[suffix];
+			
+			// http://stackoverflow.com/questions/6536244/check-if-object-is-class-type
+			if (class_isMetaClass(object_getClass(mapping))) {
+				Class class = mapping;
+				id<OCUDLClass> literalClass = [class alloc];
+				return (id)[literalClass initWithLiteral:str suffix:suffix];
+			}
+			else if ([mapping isKindOfClass:NSClassFromString(@"NSBlock")]) {
+				OCUDLBlock block = mapping;
+				return block(str, suffix);
+			}
+		}
+	}
+	
+	return str;
 }
 
 @end
